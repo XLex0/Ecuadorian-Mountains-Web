86c7b40c674b0c9f76d3657782a86baf6e6b3cd8
Nick <nick.valverde@epn.edu.ec>
2025-02-04 03:32:04 -0500

    Subir BD a la nube

--- mountainV2/configBD/configBD.php
+++ mountainV2/configBD/configBD.php
@@ -1,11 +1,18 @@
 <?php
-$servername = "localhost"; // Si es en Clever Cloud, usa el host remoto
-$username = "root"; // O el usuario configurado en phpMyAdmin
-//$password = ""; // Si tienes contraseña en XAMPP, agrégala aquí
-$dbname = "inti_cumbres"; // Debe coincidir con el nombre en phpMyAdmin
+$servername = "bgarvelirt4tpmavpych-mysql.services.clever-cloud.com"; // Si es en Clever Cloud, usa el host remoto
+$username = "u8z6exh2a03cekor"; // O el usuario configurado en phpMyAdmin
+$password = "nxDwvNtb4WgMLTirLFpv"; // Si tienes contraseña en XAMPP, agrégala aquí
+$dbname = "bgarvelirt4tpmavpych"; // Debe coincidir con el nombre en phpMyAdmin
+
+//Si queremos usar localmente
+
+#$servername = "localhost"; // Si es en Clever Cloud, usa el host remoto
+#$username = "root"; // O el usuario configurado en phpMyAdmin
+#$password = ""; // Si tienes contraseña en XAMPP, agrégala aquí
+#$dbname = "inti_cumbres"; // Debe coincidir con el nombre en phpMyAdmin
 
 //Comentar esto es solo para mi 
-$password = "admin2024"; 
+//$password = "admin2024"; 
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
